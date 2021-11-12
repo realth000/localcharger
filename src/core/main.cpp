b246da6d71212a1eeec92454aaba0d3a915b8bf2
realth000 <a598919335@163.com>
2021-11-12 12:24:08 +0800

    fix(main):修复检测系统平台错误的问题
    修复：
        修复检测系统平台错误的问题

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,14 +2,10 @@
 #include <QtWidgets/QApplication>
 #include <QFont>
 
-#if QT_VERSION <= QT_VERSION_CHECK(5, 12, 4)
-#define Q_OS_WINDOWS Q_OS_WIN
-#endif
-
 int main(int argc, char *argv[])
 {
     QFont appFont;
-#ifdef Q_OS_WINDOWS
+#if defined(Q_OS_WINDOWS) || defined(Q_OS_WIN)
     appFont.setFamily("Microsoft YaHei");
 #elif defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
     appFont.setFamily("Dejavu Sans Mono");
