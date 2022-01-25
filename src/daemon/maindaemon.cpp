c448cf2c57715e816428054f2f181615e1eb9831
realth000 <a598919335@gmail.com>
2022-01-25 22:26:04 +0800

    feat(Daemon): Add singleapplication detection
    New features:
        Add singleapplication detection.

--- src/daemon/maindaemon.cpp
+++ src/daemon/maindaemon.cpp
@@ -1,5 +1,6 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
+#include <QtCore/QSharedMemory>
 #include <QtDBus/QDBusConnection>
 #include <QtDBus/QDBusError>
 #include "daemon.h"
@@ -8,6 +9,18 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+    QSharedMemory sharedMemory("local_charger_daemon_shared_mem11");
+    if(sharedMemory.attach()){
+        qInfo() << "daemon already started";
+        // FIXME: Normally, there should NOT be detch().
+        // In fact, the attached sharedMemory will not free after exit, so
+        // detach() is needed, which performs different in KeyContainer.
+        // In this case, detach() will free sharedMemory so the latter
+        // start will success if the former one exited.
+        sharedMemory.detach();
+        exit(-2);
+    }
+    sharedMemory.create(1);
     LocalChargerDaemon d;
     QDBusConnection daemonConnection = QDBusConnection::sessionBus();
     if(!daemonConnection.registerService(DAEMON_SERVICE_NAME)){
